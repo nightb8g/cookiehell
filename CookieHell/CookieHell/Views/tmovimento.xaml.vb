48aa5142f585009692a6d468259d505c9d7ef354
dj34ever <dj_34ever@hotmail.com>
2015-02-17 12:03:38 +0000

    parte inicial movimento

---
+++ CookieHell/CookieHell/Views/tmovimento.xaml.vb
@@ -0,0 +1,46 @@
+﻿Partial Public Class tmovimento
+    Inherits UserControl
+
+    Public Sub New 
+        InitializeComponent()
+
+    End Sub
+
+
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs) Handles btnStart.Click
+
+        andarDireita(player)
+
+    End Sub
+
+    Private Sub andarDireita(img As UserControl)
+      
+        img.Margin = New Thickness(img.Margin.Left + 100, img.Margin.Top, img.Margin.Right, img.Margin.Bottom)
+    End Sub
+    Private Sub andarEsquerda(img As Object)
+        img.Margin = New Thickness(img.Margin.Left - 100, img.Margin.Top, img.Margin.Right, img.Margin.Bottom)
+    End Sub
+
+    Private Sub andarCima(img As Object)
+        img.Margin = New Thickness(img.Margin.Left, img.Margin.Top - 100, img.Margin.Right, img.Margin.Bottom)
+    End Sub
+ 
+
+    Private Sub Canvas_KeyUp(sender As Object, e As KeyEventArgs)
+        If (e.Key = Key.Up) Then
+
+        End If
+    End Sub
+
+
+ 
+    Private Sub player_KeyDown(sender As Object, e As KeyEventArgs) Handles player.KeyDown
+        If (e.Key = Key.Right) Then
+            andarDireita(player)
+        ElseIf (e.Key = Key.Left) Then
+            andarEsquerda(player)
+        ElseIf (e.Key = Key.Up) Then
+
+        End If
+    End Sub
+End Class
