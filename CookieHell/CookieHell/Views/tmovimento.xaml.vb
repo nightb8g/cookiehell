caedc8e63324087b1a529ddfd28dd25e6978c5ab
dj34ever <dj_34ever@hotmail.com>
2015-02-17 23:39:38 +0000

    Modificado o saltar

--- CookieHell/CookieHell/Views/tmovimento.xaml.vb
+++ CookieHell/CookieHell/Views/tmovimento.xaml.vb
@@ -2,67 +2,61 @@
     Inherits UserControl
     Dim direcao As Integer = 1
 
-
     Public Sub New 
         InitializeComponent()
     End Sub
 
-    Private Sub story_saltar(left, top, img)
-        '      <Storyboard x:Name="saltar">
-        '	<DoubleAnimationUsingKeyFrames Storyboard.TargetProperty="(Canvas.Top)" Storyboard.TargetName="player">
-        '		<EasingDoubleKeyFrame KeyTime="0:0:0.3" Value="140"/>
-        '		<EasingDoubleKeyFrame KeyTime="0:0:0.5" Value="190"/>
+    Private Sub storyboard_saltar(st As Storyboard)
+        ' <!--<Storyboard x:Name="saltar">
+        '	<DoubleAnimationUsingKeyFrames Storyboard.TargetProperty="(UIElement.RenderTransform).(CompositeTransform.TranslateX)" Storyboard.TargetName="image">
+        '		<EasingDoubleKeyFrame KeyTime="0:0:0.5" Value="50"/>
+        '		<EasingDoubleKeyFrame KeyTime="0:0:0.8" Value="80"/>
         '	</DoubleAnimationUsingKeyFrames>
-        '	<DoubleAnimationUsingKeyFrames Storyboard.TargetProperty="(Canvas.Left)" Storyboard.TargetName="player">
-        '		<EasingDoubleKeyFrame KeyTime="0:0:0.3" Value="60"/>
-        '		<EasingDoubleKeyFrame KeyTime="0:0:0.5" Value="110"/>
+        '	<DoubleAnimationUsingKeyFrames Storyboard.TargetProperty="(UIElement.RenderTransform).(CompositeTransform.TranslateY)" Storyboard.TargetName="image">
+        '		<EasingDoubleKeyFrame KeyTime="0:0:0.5" Value="-100"/>
+        '		<EasingDoubleKeyFrame KeyTime="0:0:0.8" Value="0"/>
         '	</DoubleAnimationUsingKeyFrames>
-        '</Storyboard>
+        '</Storyboard> -->
+
         Dim daTop As New DoubleAnimationUsingKeyFrames
         Dim daLeft As New DoubleAnimationUsingKeyFrames
         Dim keyframemidle As New EasingDoubleKeyFrame
         Dim keyframefinal As New EasingDoubleKeyFrame
 
-        Dim st As New Storyboard
-
-
-        keyframemidle.KeyTime = KeyTime.FromTimeSpan(New TimeSpan(0, 0, 3))
-        keyframefinal.KeyTime = KeyTime.FromTimeSpan(New TimeSpan(0, 0, 5))
-
 
         'Left
-        keyframemidle.Value = left + (direcao * 50)
-        keyframefinal.Value = keyframefinal.Value + (direcao * 50)
+        keyframemidle.KeyTime = KeyTime.FromTimeSpan(New TimeSpan(0, 0, 5))
+        keyframefinal.KeyTime = KeyTime.FromTimeSpan(New TimeSpan(0, 0, 10))
+        keyframemidle.Value = player_pontos.TranslateX + (direcao * 50)
+        keyframefinal.Value = keyframemidle.Value + (direcao * 80)
         daLeft.KeyFrames.Add(keyframemidle)
         daLeft.KeyFrames.Add(keyframefinal)
-
-        keyframemidle = New EasingDoubleKeyFrame
-        keyframefinal = New EasingDoubleKeyFrame
-        keyframemidle.KeyTime = KeyTime.FromTimeSpan(New TimeSpan(0, 0, 3))
-        keyframefinal.KeyTime = KeyTime.FromTimeSpan(New TimeSpan(0, 0, 5))
+        Storyboard.SetTarget(daLeft, player_pontos)
+        Storyboard.SetTargetProperty(daLeft, New PropertyPath(CompositeTransform.TranslateXProperty))
+        st.Children.Add(daLeft)
 
         'Top
-        keyframemidle.Value = top - (direcao * 50)
-        keyframefinal.Value = keyframefinal.Value + (direcao * 50)
-
+        keyframemidle = New EasingDoubleKeyFrame
+        keyframefinal = New EasingDoubleKeyFrame
+        keyframemidle.KeyTime = KeyTime.FromTimeSpan(New TimeSpan(0, 0, 5))
+        keyframefinal.KeyTime = KeyTime.FromTimeSpan(New TimeSpan(0, 0, 10))
+        keyframemidle.Value = player_pontos.TranslateY - 100
+        'keyframefinal.Value = keyframemidle.Value + 100
+        keyframefinal.Value = 200
         daTop.KeyFrames.Add(keyframemidle)
         daTop.KeyFrames.Add(keyframefinal)
+        Storyboard.SetTarget(daTop, player_pontos)
+        Storyboard.SetTargetProperty(daTop, New PropertyPath(CompositeTransform.TranslateYProperty))
 
-
-        Storyboard.SetTarget(daTop, player)
-        Storyboard.SetTargetProperty(daTop, New PropertyPath(img.Margin.Top))
-        Storyboard.SetTarget(daLeft, player)
-        Storyboard.SetTargetProperty(daLeft, New PropertyPath(img.Margin.Left))
-
-        st.Children.Add(daLeft)
         st.Children.Add(daTop)
-        st.Begin()
+        st.SpeedRatio = 10
     End Sub
 
 
-    Private Sub saltarlado(img As UserControl)
-
-        story_saltar(img.Margin.Left, img.Margin.Top, img)
+    Private Sub saltarlado()
+        Dim st As New Storyboard
+        storyboard_saltar(st)
+        st.Begin()
 
     End Sub
 
@@ -70,14 +64,13 @@
     Public Sub player_KeyDown(sender As Object, e As KeyEventArgs)
 
         If (e.Key = Key.Right) Then
-            player_pontos.TranslateX = player_pontos.TranslateX + 100
-            direcao = 1
+            player_pontos.TranslateX = player_pontos.TranslateX + 10
+            direcao = 1 'direira
         ElseIf (e.Key = Key.Left) Then
-            player_pontos.TranslateX = player_pontos.TranslateX - 100
-            direcao = -1
+            player_pontos.TranslateX = player_pontos.TranslateX - 10
+            direcao = -1 'esquerda
         ElseIf (e.Key = Key.Up) Then
-
-
+            saltarlado()
         End If
     End Sub
 
