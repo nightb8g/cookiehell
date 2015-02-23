545ee276f31b05ab80c1ffb876b611fd1ac2b4e2
dj34ever <dj_34ever@hotmail.com>
2015-02-23 15:12:33 +0000

    teste bg v2

--- CookieHell/CookieHell/MainPage.xaml.vb
+++ CookieHell/CookieHell/MainPage.xaml.vb
@@ -27,7 +27,7 @@ Partial Public Class MainPage
     End Sub
 
     Private Sub player_KeyDown(sender As Object, e As KeyEventArgs)
-        If (ContentFrame.CurrentSource.OriginalString.Contains("tmovimento")) Then
+        If (ContentFrame.CurrentSource.OriginalString.Contains("tmovimento") And Not (ContentFrame.CurrentSource.OriginalString.Contains("tmovimento2"))) Then
 
             CType(ContentFrame.Content, tmovimento).player_KeyDown(sender, e)
         End If
