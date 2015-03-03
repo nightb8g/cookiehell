57584fdcf7068f809ed0eb0f6987c743da723c81
dj34ever <dj_34ever@hotmail.com>
2015-03-03 23:20:22 +0000

    Corrigido colisão Generalizado funções movimento de objectos

--- CookieHell/CookieHell/Views/tmovimento3.xaml.vb
+++ CookieHell/CookieHell/Views/tmovimento3.xaml.vb
@@ -5,6 +5,7 @@ Partial Public Class tmovimento3
 
     Dim direcao As Double = 1
     Dim playerimgcount As Integer = 1
+
     Public Sub New()
         InitializeComponent()
 
@@ -13,22 +14,20 @@ Partial Public Class tmovimento3
     'Executes when the user navigates to this page.
     Protected Overrides Sub OnNavigatedTo(ByVal e As System.Windows.Navigation.NavigationEventArgs)
 
-
     End Sub
     Public Sub player_KeyDown(sender As Object, e As KeyEventArgs)
 
         If (e.Key = Key.Right) Then
             direcao = 1
-            mover(15, direcao)
+            mover(player, 15.0R, direcao)
         ElseIf (e.Key = Key.Left) Then
             direcao = -1
-            mover(15, direcao)
+            mover(player, 15.0R, direcao)
         ElseIf (e.Key = Key.Up) Then
             saltarlado()
         ElseIf (e.Key = Key.B) Then
             Dim rand As New Random()
             Dim val = rand.NextDouble() * (bg.ActualWidth - 200) ' para nao aparecer nas margens
-
             gerarCaixa(val)
         End If
 
@@ -41,7 +40,7 @@ Partial Public Class tmovimento3
         Dim obj1Top As Double = obj1.GetValue(Canvas.TopProperty)
         Dim obj2Top As Double = obj2.GetValue(Canvas.TopProperty)
         Dim obj1Width As Double = obj1.GetValue(Canvas.ActualWidthProperty)
-        Dim obj2Width As Double = obj2.GetValue(Canvas.WidthProperty)
+        Dim obj2Width As Double = obj2.GetValue(Canvas.ActualWidthProperty)
         Dim obj1Right As Double = obj1Left + obj1Width
         Dim obj2Right As Double = obj2Left + obj2Width
         Dim obj1Bottom As Double = obj1Top + obj1.GetValue(Canvas.ActualHeightProperty)
@@ -62,20 +61,8 @@ Partial Public Class tmovimento3
             Return False
         End If
     End Function
-    Private Sub direita(playerLeft As Double, canvasMidle As Double, bgLeft As Double, bgWidth As Double, canvasWidth As Double, distancia As Double)
-
-        If (playerLeft < canvasMidle) Then
-            player.SetValue(Canvas.LeftProperty, playerLeft + distancia)
-        ElseIf (canvasWidth - bgLeft < bgWidth) Then
-            bg.SetValue(Canvas.LeftProperty, bgLeft - distancia)
-        ElseIf (playerLeft < canvasWidth - player.ActualWidth) Then
-            player.SetValue(Canvas.LeftProperty, playerLeft + distancia)
-        End If
-        player.RenderTransform.SetValue(CompositeTransform.ScaleXProperty, direcao)
-
-
-    End Sub
-    Private Sub mover(distancia As Double, direcao As Double)
+   
+    Private Sub mover(ByRef obj As Image, distancia As Double, direcao As Double)
         'Background
         Dim bgLeft As Double = bg.GetValue(Canvas.LeftProperty)
         Dim bgTop As Double = bg.GetValue(Canvas.TopProperty)
@@ -96,20 +83,20 @@ Partial Public Class tmovimento3
         Dim canvasMidle As Double = canvasWidth / 2
 
         'MessageBox.Show(player.Source.GetValue(Media.Imaging.BitmapImage.UriSourceProperty).ToString)
-
-        If (playerimgcount = 1) Then
-            player.Source.SetValue(Media.Imaging.BitmapImage.UriSourceProperty, New Uri("/CookieHell;component/img/c_anda1.png", UriKind.RelativeOrAbsolute))
-            playerimgcount = 2
-        Else
-            player.Source.SetValue(Media.Imaging.BitmapImage.UriSourceProperty, New Uri("/CookieHell;component/img/c_anda2.png", UriKind.RelativeOrAbsolute))
-            playerimgcount = 1
+        If (obj.Equals(player)) Then
+            If (playerimgcount = 1) Then
+                player.Source.SetValue(Media.Imaging.BitmapImage.UriSourceProperty, New Uri("/CookieHell;component/img/c_anda1.png", UriKind.RelativeOrAbsolute))
+                playerimgcount = 2
+            Else
+                player.Source.SetValue(Media.Imaging.BitmapImage.UriSourceProperty, New Uri("/CookieHell;component/img/c_anda2.png", UriKind.RelativeOrAbsolute))
+                playerimgcount = 1
+            End If
         End If
-
         Select Case direcao
             Case -1
-                esquerda(playerLeft, canvasMidle, bgLeft, distancia)
+                esquerda(obj, distancia)
             Case 1
-                direita(playerLeft, canvasMidle, bgLeft, bgWidth, canvasWidth, distancia)
+                direita(obj, distancia)
             Case 0
                 'saltar
         End Select
@@ -120,17 +107,60 @@ Partial Public Class tmovimento3
 
         Next
     End Sub
-    Private Sub esquerda(playerLeft As Double, canvasMidle As Double, bgLeft As Double, distancia As Double)
+    Private Sub direita(ByRef obj As Image, distancia As Double)
+        'metodo antigo (apenas para referência)
+        'If (playerLeft < canvasMidle) Then
+        '    player.SetValue(Canvas.LeftProperty, playerLeft + distancia)
+        'End If
+        'If (canvasWidth - bgLeft < bgWidth) Then
+        '    bg.SetValue(Canvas.LeftProperty, bgLeft - distancia)
+        'ElseIf (playerLeft < canvasWidth - player.ActualWidth) Then
+        '    player.SetValue(Canvas.LeftProperty, playerLeft + distancia)
+        'End If
 
-        If (playerLeft > canvasMidle) Then
-            player.SetValue(Canvas.LeftProperty, playerLeft - distancia)
-        ElseIf (bgLeft < 0) Then
-            bg.SetValue(Canvas.LeftProperty, bgLeft + distancia)
-        ElseIf (playerLeft > 0) Then
-            player.SetValue(Canvas.LeftProperty, playerLeft - distancia)
+        Dim bgright As Double = bg.GetValue(Canvas.LeftProperty) + bg.ActualWidth
+        Dim canvasright As Double = LayoutRoot.GetValue(Canvas.LeftProperty) + LayoutRoot.ActualWidth
+        Dim objright As Double = obj.GetValue(Canvas.LeftProperty) + obj.ActualWidth
+        Dim objleft As Double = obj.GetValue(Canvas.LeftProperty)
+        Dim canvasmidle As Double = LayoutRoot.ActualWidth / 2
 
+        'bgright - canvasright > 0 = backgroud right alinhado com o layoutroot right
+        'objleft > canvasmidle = objecto depois do ponto central do layoutroot
+        If (bgright - canvasright > 0 And objleft > canvasmidle) Then
+            bg.SetValue(Canvas.LeftProperty, bg.GetValue(Canvas.LeftProperty) - distancia) 'move o bg (todos os objecto contidos)
         End If
-        player.RenderTransform.SetValue(CompositeTransform.ScaleXProperty, direcao)
+
+        'bg.ActualWidth - objright > 0 = enquando o objecto nao chegou ao fim do bg
+        If (bg.ActualWidth - objright > 0) Then
+            obj.SetValue(Canvas.LeftProperty, obj.GetValue(Canvas.LeftProperty) + distancia) 'move o player(
+        End If
+        obj.RenderTransform.SetValue(CompositeTransform.ScaleXProperty, direcao)
+
+
+    End Sub
+    Private Sub esquerda(ByRef obj As Image, distancia As Double)
+        'metodo antigo (apenas para referência)
+        'If (playerLeft > canvasMidle) Then
+        '    player.SetValue(Canvas.LeftProperty, playerLeft - distancia)
+        'End If
+        'If (bgLeft < 0) Then
+        '    bg.SetValue(Canvas.LeftProperty, bgLeft + distancia)
+        'ElseIf (playerLeft > 0) Then
+        '    player.SetValue(Canvas.LeftProperty, playerLeft - distancia)
+
+        'End If
+        Dim bgleft As Double = bg.GetValue(Canvas.LeftProperty)
+        Dim canvasleft As Double = LayoutRoot.GetValue(Canvas.LeftProperty)
+        Dim objleft As Double = obj.GetValue(Canvas.LeftProperty)
+        Dim canvasmidle As Double = LayoutRoot.ActualWidth / 2
+
+        If (canvasleft - bgleft > 0 And objleft < canvasmidle) Then
+            bg.SetValue(Canvas.LeftProperty, bg.GetValue(Canvas.LeftProperty) + distancia)
+        End If
+        If (objleft > 0) Then
+            obj.SetValue(Canvas.LeftProperty, obj.GetValue(Canvas.LeftProperty) - distancia)
+        End If
+        obj.RenderTransform.SetValue(CompositeTransform.ScaleXProperty, direcao)
 
     End Sub
 
@@ -198,8 +228,8 @@ Partial Public Class tmovimento3
         caixas.Children.Add(box)
 
     End Sub
-    Private Sub gerarEnimigo()
-
+    Private Sub gerarEnimigo(ByVal Left As Double)
+        'ToBeImplemented
     End Sub
 
     Private Sub saltarlado()
