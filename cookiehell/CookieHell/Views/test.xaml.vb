97169490478b4675177f2c0560a9b5e3e82f5859
nightb8g <daiela.jesus.siro@gmail.com>
2015-03-16 09:42:26 +0000

    fdfdf

--- cookiehell/CookieHell/Views/test.xaml.vb
+++ cookiehell/CookieHell/Views/test.xaml.vb
@@ -8,6 +8,10 @@
     'Executes when the user navigates to this page.
     Protected Overrides Sub OnNavigatedTo(ByVal e As System.Windows.Navigation.NavigationEventArgs)
 
+        '<MediaElement x:Name="mediaElement" Source="/img/demo_HD.wmv" Height="219" Canvas.Left="349" Canvas.Top="45" Width="573" AutoPlay="true" IsMuted="True" Stretch="Fill"/
+
+
+
     End Sub
 
     'Private Sub Replay(sender As Object, e As MouseButtonEventArgs) Handles play.MouseLeftButtonDown, play.MouseRightButtonDown
@@ -18,4 +22,6 @@
     '        MediaElement.Play()
     '    End If
     'End Sub
+
+
 End Class
